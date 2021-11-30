c09b65376c42cf318a3f5cbeb4ed15c4a0c0cb5d
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-11-30 21:52:43 +0530

    🤧

--- plugins/Dont_touch/filestore.py
+++ plugins/Dont_touch/filestore.py
@@ -3,6 +3,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from bot import Bot
 from config import ADMINS
 from helper_func import encode, get_message_id
+await Bot.Start
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
 async def batch(client: Client, message: Message):
