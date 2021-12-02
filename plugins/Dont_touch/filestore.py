9e477bc4817f948bc6313dabfc8f401cd1fa1efc
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-12-02 11:28:31 +0530

    🤧

--- plugins/Dont_touch/filestore.py
+++ plugins/Dont_touch/filestore.py
@@ -1,58 +1,67 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from bot import Bot
-from config import ADMINS
-from helper_func import encode, get_message_id
-
-@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
-async def batch(client: Client, message: Message):
-    while True:
-        try:
-            first_message = await client.ask(text = "Forward the First Message from DB Channel (with Quotes)..\n\nor Send the DB Channel Post Link", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
-        except:
-            return
-        f_msg_id = await get_message_id(client, first_message)
-        if f_msg_id:
-            break
-        else:
-            await first_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote = True)
-            continue
-
-    while True:
-        try:
-            second_message = await client.ask(text = "Forward the Last Message from DB Channel (with Quotes)..\nor Send the DB Channel Post link", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
-        except:
-            return
-        s_msg_id = await get_message_id(client, second_message)
-        if s_msg_id:
-            break
-        else:
-            await second_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote = True)
-            continue
-
-
-    string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
-    base64_string = await encode(string)
-    link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await second_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
-
-
-@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
-async def link_generator(client: Client, message: Message):
-    while True:
-        try:
-            channel_message = await client.ask(text = "Forward Message from the DB Channel (with Quotes)..\nor Send the DB Channel Post link", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
-        except:
-            return
-        msg_id = await get_message_id(client, channel_message)
-        if msg_id:
-            break
-        else:
-            await channel_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is not taken from DB Channel", quote = True)
-            continue
-
-    base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
-    link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await channel_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
+# (c) @MrPurple902 
+ 
+import re 
+ 
+from pyromod import listen 
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup 
+from pyrogram import Client, filters 
+from pyrogram.errors import PeerIdInvalid, MessageIdInvalid 
+ 
+@Client.on_message(filters.command("batch") & filters.private & ~filters.bot, group=3) 
+async def batch(bot:Client, update:Message): 
+ 
+    user_id = update.from_user.id 
+ 
+    post1:Message = await bot.ask(chat_id=update.chat.id, text="Please Forward The First Post From The Channel (Where I Am an admin)", timeout=360) 
+    if not post1: return 
+ 
+    if not post1.forward_from_chat: 
+ 
+        await update.reply_text("Please Forward The Message With Quotes (ie : Forwarded From ...)") 
+        return 
+ 
+    chat_id1 = post1.forward_from_chat.id 
+    try : 
+ 
+        msg_id1 = post1.forward_from_message_id 
+        await bot.get_messages( 
+            chat_id=chat_id1, 
+            message_ids=msg_id1 
+        ) 
+    except PeerIdInvalid: 
+        return await update.reply_text("Looks like Im Not A Member Of The Chat Where This Message Is Posted") 
+    except MessageIdInvalid: 
+        return await update.reply_text("Looks Like The Message You Forwarded No Longer Exists") 
+    except Exception as e: 
+        print(e) 
+        return await update.reply_text("Something Went Wrong Please Try Again Later") 
+ 
+    post2 = await bot.ask(chat_id=update.chat.id, text="Now Forward The Last Message From The Same Channel", timeout=360) 
+    if not post2 : return 
+ 
+    chat_id2 = post2.forward_from_chat.id 
+    if not chat_id1==chat_id2 : 
+        return await update.reply_text("These Two Messages Arent From The Same Chat") 
+ 
+    try : 
+ 
+        msg_id2 = post2.forward_from_message_id 
+        await bot.get_messages( 
+            chat_id=chat_id2, 
+            message_ids=msg_id2 
+        ) 
+    except PeerIdInvalid: 
+        return await update.reply_text("Looks like Im Not A Member Of The Chat Where This Message Is Posted") 
+    except MessageIdInvalid: 
+        return await update.reply_text("Looks Like The Message You Forwarded No Longer Exists") 
+    except Exception as e: 
+        print(e) 
+        return await update.reply_text("Something Went Wrong Please Try Again Later") 
+ 
+    if not msg_id1<=msg_id2: 
+        return await update.reply_text("The First Message Has To Be Posted Above The Second In The Channel To Generate A Batch") 
+ 
+    encoded = await encode(f"{str(chat_id1).replace('-100','')} {msg_id1} {msg_id2}") 
+    url = f"https://t.me/DoraFilterBot?start=a{encoded}" 
+ 
+    await update.reply_text(f"Woohoo... I've Successfully Generated A Link For Your Batch\n{url}\nPS:This Link Will Only Work As Long As I AM An Admin In The From Channel")
